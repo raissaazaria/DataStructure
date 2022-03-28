e7e6ac881beddd325679264bd8ea22adb6fc5801
raissaazaria <91534032+raissaazaria@users.noreply.github.com>
2022-03-29 00:35:24 +0700

    Update Week6Forum.cpp

--- Week6Forum.cpp
+++ Week6Forum.cpp
@@ -1,4 +1,1 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/cpplite/CPPTemplate.cpp to edit this template
- */
+
